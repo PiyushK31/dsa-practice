39a2b474c30fc8b43b411c960eddf69c406532a0
Piyushk31 <piyushkrr80580@gmail.com>
2025-07-24 13:08:20 +0530

    WIP: Save local changes before pull

--- Recursion/FactofNo.java
+++ Recursion/FactofNo.java
@@ -1,21 +1,37 @@
+// package Recursion;
+
+// public class FactofNo {
+
+//     // Recursive method to calculate factorial
+//     static int FactofNo(int n) {
+
+//         // Base case
+//         if (n == 0) {
+//             return 1;
+//         }
+
+//         // Recursive call and self work
+//         return n * FactofNo(n - 1);
+//     }
+
+//     public static void main(String args[]) {
+//         int n = 5;
+//         System.out.println(FactofNo(n)); // Directly printing result
+//     }
+// }
 package Recursion;
 
 public class FactofNo {
-
-    // Recursive method to calculate factorial
-    static int FactofNo(int n) {
-
-        // Base case
+    public static int FactofNo(int n) {
         if (n == 0) {
             return 1;
         }
 
-        // Recursive call and self work
         return n * FactofNo(n - 1);
     }
 
-    public static void main(String args[]) {
+    public static void main(String arhs[]) {
         int n = 5;
-        System.out.println(FactofNo(n)); // Directly printing result
+        System.out.println(FactofNo(n));
     }
 }
