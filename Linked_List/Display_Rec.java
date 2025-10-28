53130ee344d57a71fc4c94fe6a58d47270c0452b
Piyush Kumar <piyushkrr80580@gmail.com>
2025-10-28 09:46:37 -0700

    adding linkedList code :)

---
+++ Linked_List/Display_Rec.java
@@ -0,0 +1,29 @@
+package Linked_List;
+
+public class Display_Rec {
+
+    static void Display_Rec(Node head){
+        if(head == null) return ;
+        System.out.print(head.data + " " );
+        Display_Rec(head.next);
+    }
+    public static class Node {
+        int data;
+        Node next;
+        Node(int data){
+            this.data  = data;
+        }
+    }
+    public static void main(String args[]){
+       Node a = new Node(5);
+       Node b = new Node(3);
+       Node c = new Node(9);
+       Node d = new Node(8);
+       Node e = new Node(16);
+       a.next = b;
+       b.next = c;
+       c.next = d;
+       d.next = e;
+       Display_Rec(a);
+    }
+}
