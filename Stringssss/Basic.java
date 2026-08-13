be49006ffcddbcff3df3e1f8ed98a5064ddf4dae
Piyush Kumar <piyushkrr80580@gmail.com>
2026-08-13 06:01:41 +0000

    Basic Of Stringsss

---
+++ Stringssss/Basic.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Basic{
+    public static void main(String args[]){
+        //String Delecerition:-
+        // String name = "Piyush";
+        // System.out.println(name);
+
+        //Taking input in STR:-
+        // Scanner sc = new Scanner(System.in);
+        //this is for single line:-
+        // String name = sc.next();
+        // String name = sc.nextLine();
+        // System.out.println("Your name is:" +name);
+
+        //Concatentation(can write A String between Two String!)
+        // String firstName = "Piyush";
+        // String secName = "Kumar";
+        // String name = firstName+ " " +secName;
+        // System.out.println(name);
+
+        //length function:-
+        // String emaill = "PiyushDev12@tdx.in";
+        // System.out.println(emaill.length());
+
+        //CharAt(to find what the value at char x):-
+        // String text = "My name is Piyush";
+        // for(int i=0 ; i<text.length(); i++){
+        //     System.out.println(text.charAt(i));
+        // }
+
+        //compare:-
+        // String nameOne = "hsuyip";
+        // String nameTwo = "Piyush";
+        // if(nameOne.compareTo(nameTwo) == 0){
+        //     System.out.println("String is equal :)");
+        // }
+        // else{
+        //     System.out.println("String is not equal :(");
+        // }
+
+        //Sub-Strings:
+        // String text = "My name is piyush";
+        /*  substring(start , end);    */
+        // String name = text.substring(11 , text.length());
+        // System.out.println(name);
+    }
+}
