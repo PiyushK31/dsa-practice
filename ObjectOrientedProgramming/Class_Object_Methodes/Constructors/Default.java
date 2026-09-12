e9200a2530670002bc889dbaab88049a41e80684
PiyushK31 <piyushkrr80580@gmail.com>
2026-09-12 14:56:47 +0530

    Add Default class with a default constructor and instance variable

---
+++ ObjectOrientedProgramming/Class_Object_Methodes/Constructors/Default.java
@@ -0,0 +1,12 @@
+package ObjectOrientedProgramming.Class_Object_Methodes.Constructors;
+
+public class Default {
+    //Instance variable:-
+    int num;
+
+    //Here the defalut constructor will be created by complier
+    public static void main(String args[]){
+        Default t = new Default();
+        System.out.println(t.num);
+    }
+}
