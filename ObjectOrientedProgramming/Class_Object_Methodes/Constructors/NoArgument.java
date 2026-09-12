3e9e661fbe5e15455a13b26f97fda06de08a6596
PiyushK31 <piyushkrr80580@gmail.com>
2026-09-12 15:00:21 +0530

    no argument constructors

---
+++ ObjectOrientedProgramming/Class_Object_Methodes/Constructors/NoArgument.java
@@ -0,0 +1,13 @@
+package ObjectOrientedProgramming.Class_Object_Methodes.Constructors;
+
+public class NoArgument {
+    //no argument constructors:-
+    public NoArgument(){
+        System.out.println("True");
+    }
+
+    public static void main(String args[]){
+        // NoArgument obj = new NoArgument();
+        new NoArgument();
+    }
+}
